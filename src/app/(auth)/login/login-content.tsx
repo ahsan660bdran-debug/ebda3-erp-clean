89be4ea6858fb6d0c3479d4d76d31ec4dc54771c
Ihsan <ahsan660bdran@gmail.com>
2026-03-11 07:08:20 +0400

    login form

--- src/app/(auth)/login/login-content.tsx
+++ src/app/(auth)/login/login-content.tsx
@@ -1,9 +1,39 @@
 "use client";
 
+import { useState } from "react";
+
 export default function LoginContent() {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <div style={{padding:40}}>
-      Login Page
+    <div style={{display:"flex",justifyContent:"center",alignItems:"center",height:"100vh"}}>
+      
+      <form style={{width:"300px",display:"flex",flexDirection:"column",gap:"10px"}}>
+
+        <h2>EBDA3 Login</h2>
+
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e)=>setEmail(e.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e)=>setPassword(e.target.value)}
+        />
+
+        <button type="submit">
+          Login
+        </button>
+
+      </form>
+
     </div>
   );
 }
